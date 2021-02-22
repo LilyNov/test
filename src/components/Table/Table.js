164c91644b4412b0b675ba6af4e856ecefb71978
Liliia Novikova <kplily85@gmail.com>
2021-02-22 23:54:48 +0200

    add fetch clients

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,34 +1,52 @@
 import { useQuery } from "react-query";
+import * as Constants from "../../constants";
+console.log(Constants.GRAPHQL_API);
+console.log(Constants.GET_CLIENT_QUERY);
+
+const fetchGraphQl = async () => {
+  await fetch("https://test-task.expane.pro/api/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+            {
+    getClients {
+  id
+  firstName
+  lastName
+  phone
+  avatarUrl
+}
+}
+            `,
+    }),
+  });
+};
 
 export default function Table() {
-  const { data, status } = useQuery("contacts", () =>
-    fetch(`https://test-task.expane.pro/api/graphql`).then((res) => res.json())
-  );
+  const { data, status } = useQuery("contacts", fetchGraphQl);
 
   console.log(data);
 
   return (
-    <table class="center">
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Age</th>
-      </tr>
-      <tr>
-        <td>Jill</td>
-        <td>Smith</td>
-        <td>50</td>
-      </tr>
-      <tr>
-        <td>Eve</td>
-        <td>Jackson</td>
-        <td>94</td>
-      </tr>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>80</td>
-      </tr>
-    </table>
+    <div>
+      {status === "error" && <p>Not found anything</p>}
+
+      {status === "loading" && <p>...Loading</p>}
+
+      {status === "success" && (
+        <>
+          <ul>
+            {/* {data.getClients.map(
+              ({ id, firstName, lastName, phone, avatarUrl }) => (
+                <li key={id}></li>
+              )
+            )} */}
+          </ul>
+        </>
+      )}
+    </div>
   );
 }
---
+++ src/constants.js
@@ -0,0 +1,11 @@
+export const GRAPHQL_API = "https://test-task.expane.pro/api/graphql";
+
+export const GET_CLIENT_QUERY = `query getClients {
+    getClients {
+  id
+  firstName
+  lastName
+  phone
+  avatarUrl
+}
+}`;
